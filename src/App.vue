11d94a2c5d204fcf446c9b1e242e74a1b12fe7ed
Hibrizy Jodistira Hibatullah Sulistiyo <hibrizys@gmail.com>
2022-08-27 19:02:21 +0800

    push file

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <header>
+     <h1>sf</h1>
+    </header>
+    <body>
+      <h1>Halo</h1>
+    </body>
+    
+    <footer>
+    <h1>gd</h1>
+  </footer>
+  </main>
+
+  
+</template>
+
+<style scoped>
+
+</style>

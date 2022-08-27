71384966a3b56ac13b40acb6318054f8bbdf88ef
Hibrizy Jodistira Hibatullah Sulistiyo <hibrizys@gmail.com>
2022-08-27 21:26:41 +0800

    File Helloworld, TheWelcome, dan WelcomeItem telah dihapus

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
+
 </script>
 
 <template>
-  <main>
-    <header>
-     <h1>sf</h1>
-    </header>
-    <body>
-      <h1>Halo</h1>
-    </body>
-    
-    <footer>
-    <h1>gd</h1>
-  </footer>
-  </main>
+  <Header/>
 
   
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+
+        <h1 class="Logo" style="text-align:left">Moviendie</h1>
+
+     
+</template>
+
+<style>
+
+</style>
